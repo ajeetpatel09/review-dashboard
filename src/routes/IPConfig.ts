a2291e8b198c5e00af8e75d1d924e1d5e31f0a0e
Ajeet Patel <ajeet.patel@scrobits.com>
2024-10-21 16:17:41 +0530

    fix: backend URL change

--- src/routes/IPConfig.ts
+++ src/routes/IPConfig.ts
@@ -1,2 +1,2 @@
-export const IP = "http://localhost:8000/api/v1"
-// export const IP = "https://review-analyzer-production.up.railway.app/api/v1"
+// export const IP = "http://localhost:8000/api/v1"
+export const IP = "https://review-analyzer-production.up.railway.app/api/v1"
